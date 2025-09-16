e7b53554d12fecd6ee3fa6a1c344a633e846cc0a
Gabriel <gcs232005@gmail.com>
2025-09-16 19:09:47 -0300

    .

--- js/form.js
+++ js/form.js
@@ -1,3 +1,14 @@
+const nome = document.getElementById ( "nome" )
+const email = document.getElementById ( "email" )
+const telefone = document.getElementById ( "phone" )
+const emailSecundario = document.getElementById ( "email-sec" )
+const ideiaTexto = document.getElementById ( "about-problem" )
+const buttonForm = document.querySelector ( ".send-form-container-about-me" )
+
+( function ( ) {
+    emailjs.init ( "DwHqRsRHDTxUcEI-x" )
+} ) ( )
+
 const enviarFormulario = ( ) =>
 {
     if ( ! nome.value || ! email.value || ! ideiaTexto.value )
@@ -7,14 +18,12 @@ const enviarFormulario = ( ) =>
     }
 
     const templateParams = {
-        name: nome.value,
-        email: email.value,
-        phone: telefone.value,
-        secondary_email: emailSecundario.value || "Não informado",
-        message: ideiaTexto.value
+        from_name: nome.value,
+        from_email: email.value,
+        from_telefone: telefone.value || "Não informado",
+        from_email_secundario: emailSecundario.value || "Não informado",
+        from_message: ideiaTexto.value
     }
-
-    console.log("Dados sendo enviados:", templateParams);
 
     emailjs.send ( "service_2wp1x6o", "template_misy95p", templateParams )
         .then ( response =>
@@ -30,7 +39,7 @@ const enviarFormulario = ( ) =>
         .catch ( error =>
         {
             console.error ( "Erro ao enviar e-mail:", error )
-            alert ( "Erro ao enviar mensagem" )
+            alert ( "Erro ao enviar mensagem. Verifique o console." )
         } )
 }
 
